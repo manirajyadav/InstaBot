3be857bc7d9ef933760c013485dba61b86260a5b
Mani Raj Yadav <maniraj.yadav96@gmail.com>
2017-07-10 22:17:59 +0530

    almost done

--- instabot.py
+++ instabot.py
@@ -42,19 +42,19 @@ def self_info():
 def get_user_id(insta_username):
     # setting endpoint and creating and accessing json object
     request_url = (base_url+'users/search?q=%s&access_token=%s') % (insta_username , access_token)
-    print 'GET request url : %s' % (request_url)
-    user_id= requests.get(request_url).json()
+    print 'GET request url : %s' % request_url
+    user_id = requests.get(request_url).json()
     # checking whether the request is successful
-    if user_id['meta']['code']==200:
+    if user_id['meta']['code'] == 200:
         # checking whether the user exist
         if len(user_id['data']):
             return user_id['data'][0]['id']
         else:
-
             return None
     # request unsuccessful
     else:
         print "Status code other than 200 received."
+
 
 # function for getting info of user by its instagram username
 def get_user_info(insta_username):
@@ -62,7 +62,7 @@ def get_user_info(insta_username):
     user_id= get_user_id(insta_username)
 
     # if user does not exist
-    if user_id==None:
+    if user_id == None:
         print "User with the given name does not exist.You are being sent back to the HOME!"
         start_bot()
     # making request and accessing obtained json object
@@ -85,17 +85,20 @@ def get_user_info(insta_username):
     else:
         print 'Status code other than 200 received!'
 
-
+# function to get own posts
 def get_own_post():
+    # setting up endpoint url and accessing json object
     request_url = (base_url + 'users/self/media/recent/?access_token=%s') % (access_token)
     print 'GET request url : %s' % (request_url)
     own_media = requests.get(request_url).json()
-
+    # if request is successful
     if own_media['meta']['code'] == 200:
+        # if some posts exist
         if len(own_media['data']):
             c= True
+            # loop to choose any post other than latest
             while c:
-                answer= raw_input('Do you want to get the latest post?Reply: Y/N')
+                answer= raw_input('Do you want to get the latest post? Reply: Y/N')
                 if answer.upper()== 'Y':
                     x=0
                     c= False
@@ -267,31 +270,54 @@ def delete_negative_comment(insta_username):
     if comment_info['meta']['info']==200:
         # if comment exists
         if len(comment_info['data']):
-            # iterating over all comments
-            for x in range(0,len(comment_info['data'])):
-                # getting comment id
-                comment_id=comment_info['data'][x]['id']
-                # getting comment text
-                comment_text=comment_info['data'][x]['text']
-                # analysing comment by 'TextBlob'
-                blob=TextBlob(comment_text,analyzer=NaiveBayesAnalyzer())
-                # checking whether sentiments of comment are more negative than positive
-                if (blob.sentiment.p_neg>blob.sentiment.p_pos):
-                    print "Negative comment:%s" %(comment_text)
-                    # setting up endpoint url
-                    delete_url = (base_url+ 'media/%s/comments/%s/?access_token=%s') % (media_id, comment_id, access_token)
-                    print  "DELETE request url:%s" %(delete_url)
-                    # accessing json object
-                    delete_info = requests.delete( delete_url ).json()
-                    # check whether request is successful
-                    if delete_info['meta']['code']==200:
-                        print 'Comment successfully deleted.'
-                    # request not successful
+            ans= raw_input('Do you want to delete comments containing a specific words? Reply Y/N')
+            if ans.upper()=='Y':
+                del_word= raw_input('Enter the Word!!')
+                for x in range( 0, len( comment_info['data'] ) ):
+                    # getting comment id
+                    comment_id = comment_info['data'][x]['id']
+                    # getting comment text
+                    comment_text = comment_info['data'][x]['text']
+                    if del_word in comment_text:
+                        print "Comment containing the given word:"+ colored(comment_text, 'red')
+                        # setting up endpoint url
+                        delete_url = (base_url + 'media/%s/comments/%s/?access_token=%s') % (
+                        media_id, comment_id, access_token)
+                        print  "DELETE request url:%s" % (delete_url)
+                        # accessing json object
+                        delete_info = requests.delete( delete_url ).json()
+                        # check whether request is successful
+                        if delete_info['meta']['code'] == 200:
+                            print 'Comment successfully deleted.'
+                        # request not successful
+                        else:
+                            print 'Unable to delete comment.'
+            else:
+                # iterating over all comments
+                for x in range(0,len(comment_info['data'])):
+                    # getting comment id
+                    comment_id=comment_info['data'][x]['id']
+                    # getting comment text
+                    comment_text=comment_info['data'][x]['text']
+                    # analysing comment by 'TextBlob'
+                    blob=TextBlob(comment_text,analyzer=NaiveBayesAnalyzer())
+                    # checking whether sentiments of comment are more negative than positive
+                    if (blob.sentiment.p_neg>blob.sentiment.p_pos):
+                        print "Negative comment:%s" %(comment_text)
+                        # setting up endpoint url
+                        delete_url = (base_url+ 'media/%s/comments/%s/?access_token=%s') % (media_id, comment_id, access_token)
+                        print  "DELETE request url:%s" %(delete_url)
+                        # accessing json object
+                        delete_info = requests.delete( delete_url ).json()
+                        # check whether request is successful
+                        if delete_info['meta']['code']==200:
+                            print 'Comment successfully deleted.'
+                        # request not successful
+                        else:
+                            print 'Unable to delete comment.'
+                    # positive sentiments are greater than negative sentiments!
                     else:
-                        print 'Unable to delete comment.'
-                # positive sentiments are greater than negative sentiments!
-                else:
-                    print "Positive comment:%s" %comment_text
+                        print "Positive comment:%s" %comment_text
         # no comments found
         else:
             print "there are no comments on this post yet."
@@ -357,6 +383,81 @@ def list_own_like():
             print colored('Post-Id:%s\n%s from %s.\n','blue') %(own_likes['data'][x]['id'],own_likes['data'][x]['type'],own_likes['data'][x]['user']['full_name'],)
 
 
+# function to get a post with least likes
+def least_like(insta_username):
+    # calling function to get user-id
+    user_id = get_user_id( insta_username )
+    # if user doesnt exit
+    if user_id == None:
+        print 'User does not exist!'
+        start_bot()
+    # making request and accessing obtained json object
+    request_url = (base_url + 'users/%s/media/recent/?access_token=%s') % (user_id, access_token)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get( request_url ).json()
+    # if request is successful
+    if user_media['meta']['code'] == 200:
+        # if posts exist
+        if len(user_media['data']):
+            least=user_media['data'][0]['likes']['count']
+            c=0
+            for x in range (1,len(user_media['data'])):
+                likes= user_media['data'][x]['likes']['count']
+                if likes < least:
+                    least = likes
+                    c=x
+
+            print "\nPost with least likes is:\nId=%s\nlikes=%s" %(user_media['data'][c]['id'], least)
+            print "Caption:%s" %(user_media['data'][c]['caption']['text'])
+            image_name = user_media['data'][c]['id'] + '.jpeg'
+            image_url = user_media['data'][c]['images']['standard_resolution']['url']
+            # downloading the post
+            urllib.urlretrieve( image_url, image_name )
+            print 'And the post has been downloaded!!'
+
+# search for a post with a particular tag
+def search_post_via_tag(insta_username, search_tag):
+    # calling function to get user-id
+    user_id = get_user_id( insta_username )
+    # if user doesnt exit
+    if user_id == None:
+        print 'User does not exist!'
+        start_bot()
+    # making request and accessing obtained json object
+    request_url = (base_url + 'users/%s/media/recent/?access_token=%s') % (user_id, access_token)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get( request_url ).json()
+    # if request is successful
+    if user_media['meta']['code'] == 200:
+        # if posts exist
+        if len( user_media['data'] ):
+            id = []
+            for x in range(len(user_media['data'])):
+                if search_tag in user_media['data'][x]['tags']:
+                    id.append(user_media['data'][x]['id'])
+
+            if len(id):
+                for y in id:
+                    request_url= base_url+ "media/%s?access_token=%s" %(y,access_token)
+                    print "GET request url:%s" %(request_url)
+                    tag_media = requests.get(request_url).json()
+
+                    print 'Tag: '+ colored('#','blue')+colored(search_tag,'blue')
+                    print '\nPost-Id:' + colored(y,'blue')
+                    print '\nCaption:%s' % (tag_media['data']['caption']['text'])
+                    image_name = y+'.jpeg'
+                    image_url = tag_media['data']['images']['standard_resolution']['url']
+                    # downloading the post
+                    urllib.urlretrieve( image_url, image_name )
+                    print '\nThe post has been downloaded!!'
+            else:
+                print "No media with '%s' tag found!!" %(search_tag)
+        else:
+            print "No media exist!!"
+    else:
+        print 'Status code other than 200 received'
+
+
 
 # starting the application
 def start_bot():
@@ -367,7 +468,7 @@ def start_bot():
     print 'Try it yourself!!'
     while True:
         print '\n'
-        print 'Here are your menu options:'
+        print 'Here are your menu options:\n'
         print "1.Get your own details\n"
         print "2.Get details of a user by username\n"
         print "3.Get your own recent post\n"
@@ -378,7 +479,9 @@ def start_bot():
         print "8.Make a comment on the recent post of a user\n"
         print "9.Delete negative comments from the recent post of a user\n"
         print "10.Get list of posts liked by the User.\n"
-        print "11.Exit."
+        print "11.Get the post with least like.\n"
+        print "12.Search a post with a particular tag."
+        print "13.Exit.\n"
 
 
         choice = int(raw_input("Enter you choice: "))
@@ -409,8 +512,14 @@ def start_bot():
            delete_negative_comment(insta_username)
         elif choice == 10:
             list_own_like()
-
-        elif choice == 11:
+        elif choice== 11:
+            insta_username = raw_input( "Enter the username of the user: " )
+            least_like(insta_username)
+        elif choice== 12:
+            insta_username = raw_input( "Enter the username of the user: " )
+            search_tag = raw_input("Enter the tag you are looking for.")
+            search_post_via_tag(insta_username,search_tag)
+        elif choice == 13:
             exit()
 
         else:
